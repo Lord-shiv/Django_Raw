3f74a32ecfce27ca1c30d5605e8e3d661b6717e7
shivam <shivamchouhan728@gmail.com>
2021-03-17 13:08:58 +0530

    adds categories and buttons styling

--- Todo/todo_app/admin.py
+++ Todo/todo_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TodoItem
+from .models import TodoItem, Category
 
 # Register your models here.
 admin.site.register(TodoItem)
+admin.site.register(Category)
