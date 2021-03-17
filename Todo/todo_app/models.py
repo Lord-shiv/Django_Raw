d3f9248288a0c0a74753c6baa8ac899c31edc857
shivam <shivamchouhan728@gmail.com>
2021-03-17 13:08:58 +0530

    adds categories and buttons styling

--- Todo/todo_app/models.py
+++ Todo/todo_app/models.py
@@ -4,22 +4,19 @@ from django.utils import timezone
 # Create your models here.
 
 
-class Category(models.Model):  # The Category table name that inherits models.Model
-    name = models.CharField(max_length=100)  # Like a varchar
-
-    class Meta:
-        verbose_name = "Category"
-        verbose_name_plural = "Categories"
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Title")
 
     def __str__(self):
         return self.name
 
 
 class TodoItem(models.Model):
-    author = models.CharField(max_length=50, default="User", blank=True)
+    name = models.CharField(max_length=50, default="User", blank=True)
     content = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default="Soon")
 
     def __str__(self):
-        return self.author  # name to be shown when called
+        return self.name  # name to be shown when called
