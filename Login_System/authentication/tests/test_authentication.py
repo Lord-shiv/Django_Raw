6f21b72091871e52441b23c124a8afe76fc53388
shivam <shivamchouhan728@gmail.com>
2021-04-03 18:03:09 +0530

    adds tests for Login user

--- Login_System/authentication/tests/test_authentication.py
+++ Login_System/authentication/tests/test_authentication.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class BaseTest(TestCase):
     def setUp(self):
         self.register_url = reverse("register")
+        self.login_url = reverse("login")
         self.user = {
             "email": "testemail@gmail.com",
             "username": "username",
@@ -69,3 +71,35 @@ class RegisterTest(BaseTest):
         self.client.post(self.register_url, self.user, format="text/html")
         response = self.client.post(self.register_url, self.user, format="text/html")
         self.assertEqual(response.status_code, 400)
+
+
+class LoginTest(BaseTest):
+    def test_can_access_page(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "auth/login.html")
+
+    def test_login_success(self):
+        self.client.post(self.register_url, self.user, format="text/html")
+        user = User.objects.filter(email=self.user["email"]).first()
+        user.is_active = True
+        user.save()
+        response = self.client.post(self.login_url, self.user, format="text/html")
+        self.assertEqual(response.status_code, 302)
+
+    def test_cant_login_with_unverified_email(self):
+        self.client.post(self.register_url, self.user, format="text/html")
+        response = self.client.post(self.login_url, self.user, format="text/html")
+        self.assertEqual(response.status_code, 401)
+
+    def test_cant_login_with_no_username(self):
+        response = self.client.post(
+            self.login_url, {"password": "passwped", "username": ""}, format="text/html"
+        )
+        self.assertEqual(response.status_code, 401)
+
+    def test_cant_login_with_no_password(self):
+        response = self.client.post(
+            self.login_url, {"username": "passwped", "password": ""}, format="text/html"
+        )
+        self.assertEqual(response.status_code, 401)
